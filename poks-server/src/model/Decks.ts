aa6b8d17ef6bfd3c899bfd4644ec8cec31bbb4dd
Adrian Tosca <adrian.tosca@tradeshift.com>
2020-08-16 11:41:16 +0300

    add semaphore and vote decks

--- poks-server/src/model/Decks.ts
+++ poks-server/src/model/Decks.ts
@@ -3,7 +3,9 @@ export enum DeckKind {
     ModifiedFibonacci,
     PowersOf2,
     Sequential,
-    TShirts
+    TShirts,
+    Semaphore,
+    Vote
 }
 
 export interface Deck {
@@ -16,27 +18,37 @@ export class DeckRepository {
     private static readonly Fibonacci = {
         id: 'Fibonacci',
         name: 'Fibonacci',
-        texts: ['?', '0', '1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '∞', '☕']
+        texts: ['🤔', '0', '1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '∞', '☕']
     }
     private static readonly ModifiedFibonacci = {
         id: 'ModifiedFibonacci',
         name: 'Modified Fibonacci',
-        texts: ['?', '0', '½', '1', '2', '3', '5', '8', '13', '20', '40', '100', '∞', '☕']
+        texts: ['🤔', '0', '½', '1', '2', '3', '5', '8', '13', '20', '40', '100', '∞', '☕']
     }
     private static readonly PowersOf2 = {
         id: 'PowersOf2',
         name: 'Powers of 2',
-        texts: ['?', '0', '1', '2', '4', '8', '16', '32', '64', '128', '∞']
+        texts: ['🤔', '0', '1', '2', '4', '8', '16', '32', '64', '128', '∞', '☕']
     }
     private static readonly Sequential = {
         id: 'Sequential',
         name: 'Sequential',
-        texts: ['?', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '∞', '☕']
+        texts: ['🤔', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '∞', '☕']
     }
     private static readonly TShirts = {
         id: 'TShirts',
         name: 'T-Shirts',
-        texts: ['?', 'XS', 'S', 'M', 'L', 'XL', '∞', '☕']
+        texts: ['🤔', 'XS', 'S', 'M', 'L', 'XL', '∞', '☕']
+    }
+    private static readonly Semaphore = {
+        id: 'Semaphore',
+        name: 'Semaphore',
+        texts: ['🟩', '🟨', '🟥']
+    }
+    private static readonly Vote = {
+        id: 'Vote',
+        name: 'Vote',
+        texts: ['👍', '🤔', '👎']
     }
 
     static of(kind: DeckKind): Deck {
@@ -46,6 +58,8 @@ export class DeckRepository {
             case DeckKind.PowersOf2: return this.PowersOf2
             case DeckKind.Sequential: return this.Sequential
             case DeckKind.TShirts: return this.TShirts
+            case DeckKind.Semaphore: return this.Semaphore
+            case DeckKind.Vote: return this.Vote
             default: {
                 console.error(`no such deck: ${kind}`)
                 return this.Fibonacci
